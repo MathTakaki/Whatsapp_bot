a1cc332bcde70c80b87b2004e33b94fb98afbd64
Matheus Yuuji <yuuji061@usp.br>
2022-11-14 19:36:11 -0300

    Captura de mensagens

---
+++ whatsapp/main.py
@@ -0,0 +1,31 @@
+import pyautogui as pt
+from time import sleep
+import pyperclip
+import random
+
+sleep(3)
+
+position1 = pt.locateOnScreen("whatsapp/smiley_paperclip.png", confidence=.6)
+x = position1[0]
+y = position1[1]
+
+#Get message
+def get_message():
+    global x, y
+    
+    position = pt.locateOnScreen("whatsapp/smiley_paperclip.png", confidence=.6)
+    x = position[0]
+    y = position[1]
+    pt.moveTo(x,y, duration=.05) #only for mac, in windows its okay noto to declar the duration
+    pt.moveTo(x + 60, y - 60, duration=.5)
+    pt.tripleClick()
+    pt.rightClick()
+    pt.moveRel(50,-221)
+    pt.click()
+    whatsapp_message = pyperclip.paste()
+    pt.click()
+    print("Message received: " + whatsapp_message)
+    
+    return whatsapp_message
+    
+get_message()
