89a7dc0875d8b081be245a77a4cc78174574dba4
Matheus Yuuji <yuuji061@usp.br>
2022-11-14 21:29:21 -0300

    Check for new messages

--- whatsapp/main.py
+++ whatsapp/main.py
@@ -3,8 +3,10 @@ from time import sleep
 import pyperclip
 import random
 
-sleep(3)
-
+sleep(2)
+pt.moveTo(25,315)
+pt.click()
+sleep(1)
 position1 = pt.locateOnScreen("whatsapp/smiley_paperclip.png", confidence=.6)
 x = position1[0]
 y = position1[1]
@@ -44,6 +46,47 @@ def post_response(message):
 
 
 # Processes response
-def
+def processes_response(message):
+    random_no = random.randrange(3)
     
-post_response(get_message())
+    if "?" in str(message).lower():
+        return "Bot japoronga teste 123"
+    else:
+        if random_no == 0:
+            return "cholha"
+        elif random_no == 1:
+            return "ablubluble"
+        else:
+            return "garay"
+
+
+# chech new messages
+def check_for_new_messages():
+    pt.moveTo(x + 50, y - 45, duration=.5)
+    
+    while True:
+        #continuously checks
+        try:
+            position = pt.locateOnScreen("whatsapp/green_circle.png", confidence=.75)
+            
+            if position is not None:
+                pt.moveTo(position)
+                pt.moveRel(-100,0)
+                pt.click()
+                sleep(.5)
+            
+        except(Exception):
+            print("No new other users with new messages located")
+        
+        if pt.pixelMatchesColor(int(x + 50), int(y - 45),(32,44,51), tolerance = 10):
+            print("is the gray")
+            processed_message = processes_response(get_message())
+            post_response(processed_message)
+        else:
+            print("Searching messages...")
+        sleep(5)
+
+check_for_new_messages()
+#processed_message = processes_response(get_message())
+#post_response(processed_message)
+
